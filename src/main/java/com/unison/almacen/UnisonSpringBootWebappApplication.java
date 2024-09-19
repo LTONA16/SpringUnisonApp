6a65674d3b1102d2794d6a72c456501a923e4790
Tona <luisftona@gmail.com>
2024-09-19 12:00:16 -0700

    proyect init

---
+++ src/main/java/com/unison/almacen/UnisonSpringBootWebappApplication.java
@@ -0,0 +1,13 @@
+package com.unison.almacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnisonSpringBootWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnisonSpringBootWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unison/almacen/UnisonSpringBootWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unison.almacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnisonSpringBootWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
